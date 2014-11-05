01744ade5a007001a66e119035269209c3742bb9
Simon Hedlund <sermonhedlund@gmail.com>
2014-11-05 21:28:17 +0100

    meny med knappar som leder till nya xml-filer

---
+++ startmenu/src/com/example/startmenu/MenuTwo.java
@@ -0,0 +1,15 @@
+package com.example.startmenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuTwo extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menu2);
+	}
+
+}
